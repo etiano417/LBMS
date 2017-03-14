b2315e657476408ed684ade62fbc0a9396e36fa7
tjb4578 <bankst42@gmail.com>
2017-03-13 23:40:58 -0400

    Created Visitor Class

---
+++ src/VisitorRegistry/Visitor.java
@@ -0,0 +1,13 @@
+package VisitorRegistry;
+
+/**
+ * Created by Tyler on 3/13/2017.
+ */
+ 
+public class Visitor {
+    private String firstName;
+    private String lastName;
+    private String address;
+    private String phoneNumber;
+    private int id;
+}
