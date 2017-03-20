6f499a934d2a8992d11da13c7cc0302a1bc34865
Tyler Banks <tjb4578@g.rit.edu>
2017-03-20 11:34:43 -0400

    Implemented VisitorRegistry, Visitor, and Visit.  Needs discussion

--- src/VisitorRegistry/Visitor.java
+++ src/VisitorRegistry/Visitor.java
@@ -1,5 +1,9 @@
 package VisitorRegistry;
 
+import Transaction.Borrow;
+
+import java.util.ArrayList;
+
 /**
  * Created by Tyler on 3/13/2017.
  */
@@ -9,6 +13,47 @@ public class Visitor {
     private String lastName;
     private String address;
     private String phoneNumber;
-    private int id;
-    List<Borrow> borrowing = new ArrayList<Borrow>();
+    private long visitorID;
+    private ArrayList<Borrow> borrows = new ArrayList<>();
+
+    private static long idTracker;
+
+
+    /**
+     * Visitor constructor
+     * @param first - First name
+     * @param last - Last name
+     * @param add - Address
+     * @param phone - Phone number
+     */
+    public Visitor(String first, String last, String add, String phone) {
+        firstName = first;
+        lastName = last;
+        address = add;
+        phoneNumber = phone;
+        borrows = new ArrayList<Borrow>();
+        visitorID = idTracker++;
+    }
+
+    public void addBorrow(Borrow borrow) {
+        borrows.add(borrow);
+    }
+
+    public void removeBorrow() {
+
+    }
+
+    public long getVisitorID() {
+        return visitorID;
+    }
+
+    public ArrayList<Borrow> getBorrowing() {
+        ArrayList<Borrow> borrowing = new ArrayList<>(5);
+        for (Borrow b : borrows) {
+            if () {              //TODO: If the borrow's state is not 'Complete'
+                borrowing.add(b);
+            }
+        }
+        return borrowing;
+    }
 }
--- src/VisitorRegistry/VisitorRegistry.java
+++ src/VisitorRegistry/VisitorRegistry.java
@@ -1,7 +1,11 @@
 package VisitorRegistry;
 
-import BookRegistry.Book;
+import Transaction.*;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.util.ArrayList;
 import java.util.Collection;
 
 /**
@@ -9,28 +13,98 @@ import java.util.Collection;
  */
 public class VisitorRegistry {
     private Collection<Visitor> visitors;
+    private Collection<Visit> visits;
 
-    public void beginVisit(int id){
 
+    /**
+     * The visitor with the given ID begins a visit.  A visit object is created.
+     * The visit is given the visitor ID, the date, and the starting time.
+     * @param id - The ID of the visitor in the collection
+     */
+    public void beginVisit(int id, LocalDateTime beginTime){
+        LocalDate visitDate = beginTime.toLocalDate();
+        LocalTime visitTime = beginTime.toLocalTime();
+        Visit newVisit = new Visit(id, visitDate, visitTime);
+        visits.add(newVisit);
     }
 
-    public void endVisit(int id){
-
+    /**
+     * The visitor's visit ends.  The visitor's ongoing visit is given a
+     * departure time.
+     * @param id - The ID of the visitor in the collection
+     */
+    public void endVisit(int id, LocalTime time){
+        for (Visit v : visits) {
+            if (v.getVisitorID() == id && v.isOngoing()) {
+                v.setDepartureTime(time);
+            }
+        }
     }
 
-    public void payFine(int id, int amount){
-
+    /**
+     * The specified visitor borrows a book from the library.
+     * A new Borrow object is created with an initial state of Ongoing.
+     * @param id - The ID of the visitor
+     * @param borrow - The transaction to be added to the visitor's transactions
+     */
+    public void borrowBook(int id, Borrow borrow){
+        for (Visitor v : visitors) {
+            if (v.getVisitorID() == id && v.getBorrowing().size() < 5) {
+                borrow.setState(borrow.getOngoing());  //Placeholder for borrowState setter
+                v.addBorrow(borrow);
+            }
+        }
     }
 
-    public void borrowBook(int id, Book book){
-
+    /**
+     * The specified visitor returns a checked out book to the library.
+     * The state of the Borrow transaction becomes Complete
+     * @param id - Visitor id
+     * @param borrow The borrow transaction
+     */
+    public void returnBook(int id, Borrow borrow){
+        for (Visitor v : visitors) {
+            if (v.getVisitorID() == id) {
+                borrow.setState(borrow.getComplete());  //Placeholder for borrowState setter
+            }
+        }
     }
 
-    public void returnBook(int id, Book book){
-
+    /**
+     * The visitor pays a certain amount towards a fine
+     * @param id - Visitor id
+     * @param fine - The fine to be paid
+     * @param amount - The amount paid towards the fine
+     */
+    public void payFine(int id, Fine fine, int amount) {
+        for (Visitor v : visitors) {
+            if (v.getVisitorID() == id) {
+                fine.fee -= amount;
+                if (fine.fee <= 0) {
+                    fine.paid = true;
+                }
+            }
+        }
     }
 
-    public void findBorrowedBooks(int id){
+    /**
+     * Returns an ArrayList of the visitor's ongoing Borrow transactions.
+     * @param id
+     */
+    public ArrayList<Borrow> findBorrowedBooks(int id){
+        for (Visitor v : visitors) {
+            if (v.getVisitorID() == id) {
+                return v.getBorrowing();
+            }
+        }
+        return null;
+    }
 
+    /**
+     * Create a new visitor and add it to the collection
+     */
+    public void RegisterVisitor(String firstName, String lastName, String address, String phoneNumber) {
+
+        visitors.add(new Visitor(firstName, lastName, address, phoneNumber));
     }
 }
