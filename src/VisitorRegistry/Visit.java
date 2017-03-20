d4e4007ac12716629e189704cb578eaea10fa146
Tyler Banks <tjb4578@g.rit.edu>
2017-03-20 14:20:18 -0400

    Implemented Visit, Visitor, and VisitorRegistry classes.  Needs discussion.

--- src/VisitorRegistry/Visit.java
+++ src/VisitorRegistry/Visit.java
@@ -9,5 +9,25 @@ public class Visit {
     private LocalDate dateOfVisit;
     private LocalTime timeOfArrival;
     private LocalTime timeOfDeparture;
+    private boolean ongoing;
 
+    public Visit(int visId, LocalDate visitDate, LocalTime time) {
+        visitorID = visId;
+        dateOfVisit = visitDate;
+        timeOfArrival = time;
+        ongoing = true;
+    }
+
+    public void setDepartureTime(LocalTime time) {
+        timeOfDeparture = time;
+        ongoing = false;
+    }
+
+    public int getVisitorID() {
+        return visitorID;
+    }
+
+    public boolean isOngoing() {
+        return ongoing;
+    }
 }
--- src/VisitorRegistry/Visitor.java
+++ src/VisitorRegistry/Visitor.java
@@ -3,17 +3,53 @@ package VisitorRegistry;
 import Transaction.Borrow;
 
 import java.util.ArrayList;
-import java.util.List;
 
 /**
  * Created by Tyler on 3/13/2017.
  */
- 
+
 public class Visitor {
     private String firstName;
     private String lastName;
     private String address;
     private String phoneNumber;
-    private int id;
-    List<Borrow> borrowing = new ArrayList<Borrow>();
+    private long visitorID;
+    private ArrayList<Borrow> borrows = new ArrayList<>();
+
+    private static long idTracker = 0;
+
+
+    /**
+     * Visitor constructor
+     * @param first - First name
+     * @param last - Last name
+     * @param add - Address
+     * @param phone - Phone number
+     */
+    public Visitor(String first, String last, String add, String phone) {
+        firstName = first;
+        lastName = last;
+        address = add;
+        phoneNumber = phone;
+        borrows = new ArrayList<Borrow>();
+        visitorID = idTracker++;
+    }
+
+    public void addBorrow(Borrow borrow) {
+        borrows.add(borrow);
+    }
+
+    public long getVisitorID() {
+        return visitorID;
+    }
+
+    public ArrayList<Borrow> getBorrowing() {
+        ArrayList<Borrow> borrowing = new ArrayList<>(5);
+        for (Borrow b : borrows) {
+            if () {                                                    //TODO: If the borrow's state is not 'Complete'
+                borrowing.add(b);
+            }
+        }
+        return borrowing;
+    }
 }
