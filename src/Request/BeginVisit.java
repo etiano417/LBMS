bc2616de256fbdd721e33e80b30d9691ba59c4cb
Eric Tiano <etiano417@gmail.com>
2017-03-21 20:23:31 -0400

    Added request for BeginVisit

--- src/Request/BeginVisit.java
+++ src/Request/BeginVisit.java
@@ -15,12 +15,24 @@ public class BeginVisit implements Request{
         visitorID = _visitorID;
     }
 
-    //needs to know if a visitor is already visiting when a visit occurs
+    //TODO needs to know if a visitor is already visiting when a visit occurs
     public List<Object> executeCommand(){
-        LocalDateTime dt = LBMS.clock.getDateTime();
-        LBMS.vr.beginVisit(visitorID,dt);
+        List<Object> output = new ArrayList<>();
 
-        List<Object> output = new ArrayList<Object>();
+        LocalDateTime dt = LBMS.clock.getDateTime();
+        String result = LBMS.vr.beginVisit(Integer.toString(visitorID),dt);
+
+        if(result.equals("duplicate")){
+            output.add(new Problem("duplicate","A visitor with the id is already visiting the library."));
+            return output
+        }
+
+        if(result.equals("invalid id")){
+            output.add(new Problem("invalid-id","The specified visitor ID is not a valid ID or has not been assigned " +
+                    "to any registered visitor."));
+        }
+
+        output = new ArrayList<Object>();
         output.add(visitorID);
         output.add(dt.toLocalDate());
         output.add(dt.toLocalTime());
